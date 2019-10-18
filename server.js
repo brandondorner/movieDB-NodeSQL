5346e7febad625a3907ad199e66cf2a410b0760a
brandondorner <51007432+brandondorner@users.noreply.github.com>
2019-10-18 18:46:51 -0500

    Attached server to html/ejs file

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mysql = require('mysql')
+
+
 const app = express()
+
+
 
 app.listen('3000', () => {
     console.log('server running on port 3000')
+})
+
+//set up express server
+app.set('views', './public' )
+//telling server to use ejs
+app.set('view engine', 'ejs')
+//telling server js files will be in public folder
+app.use(express.static('public'))
+//ablity to accept url parameters
+app.use(express.urlencoded({ extended: true}))
+
+app.get('/', (req, res) => {
+    res.render('index')
 })
